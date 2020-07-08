b3c7376139d9b41929a64a4e33199d859f016efe
AbAleem003 <aleem657@users.noreply.github.com>
2020-07-08 14:42:39 +0500

    Assignment
    Assignment

---
+++ Assignment5A_Aleem003/Assets/Scripts/MENU_MANAGER.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU_MANAGER : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(MAIN());        
+    }
+    IEnumerator MAIN()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main");
+    }
+    
+}
---
+++ Assignment5A_Aleem003/Assets/Scripts/ROTATOR.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROTATOR : MonoBehaviour
+{
+    public float X,Y,Z,SPEED;
+    void Update()
+    {
+        gameObject.transform.Rotate(X*SPEED,Y* SPEED, Z* SPEED);
+    }
+}
